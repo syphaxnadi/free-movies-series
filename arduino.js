b120800c2ea492f93d0af09c0fef49fb8b1079fd
syphaxnadi <128228494+syphaxnadi@users.noreply.github.com>
2023-04-03 19:22:48 +0100

    Add files via upload

--- arduino.js
+++ arduino.js
@@ -1,19 +1,52 @@
 const form = document.querySelector('.search-form');
 const input = form.querySelector('input');
 const cards = document.querySelectorAll('.cards article');
+const categoryButtons = document.querySelectorAll('.categories .btn');
+const loginButton = document.getElementById('login-button');
 
-form.addEventListener('submit', function(event) {
-	event.preventDefault(); // Prevent the form from submitting and refreshing the page
-	
-	const searchTerm = input.value.toLowerCase(); // Get the search term and convert to lowercase
-	
-	cards.forEach(function(card) {
-		const title = card.querySelector('h2').textContent.toLowerCase(); // Get the title of the card and convert to lowercase
-		
-		if (title.includes(searchTerm)) {
-			card.style.display = 'block'; // Show the card if the search term is found in the title
-		} else {
-			card.style.display = 'none'; // Hide the card if the search term is not found in the title
-		}
+categoryButtons.forEach(button => {
+	button.addEventListener('click', () => {
+	  // Remove the 'active' class from all buttons
+	  categoryButtons.forEach(btn => btn.classList.remove('active'));
+	  // Add the 'active' class to the clicked button
+	  button.classList.add('active');
+  
+	  // Show or hide cards based on the clicked button's category
+	  const category = button.dataset.category;
+  
+	  cards.forEach(card => {
+		// If the card belongs to the clicked category or the category is "all", show it, otherwise hide it
+	    if (category === 'all') {
+			card.style.display = 'block';
+		  } else if (card.dataset.category === category) {
+			card.style.display = 'block';
+		  } else {
+			card.style.display = 'none';
+		  }
+	  });
 	});
-});
+  });
+  
+  const searchForm = document.querySelector('.search-form');
+  const searchInput = searchForm.querySelector('input');
+  const searchBtn = searchForm.querySelector('button');
+  
+  searchBtn.addEventListener('click', e => {
+	e.preventDefault();
+  
+	const searchTerm = searchInput.value.trim().toLowerCase();
+  
+	cards.forEach(card => {
+	  const title = card.querySelector('h2').textContent.trim().toLowerCase();
+  
+	  if (title.includes(searchTerm)) {
+		card.style.display = 'block';
+	  } else {
+		card.style.display = 'none';
+	  }
+	});
+  });
+  vloginButton.addEventListener('click', () => {
+	// Handle login functionality here
+  });
+  
