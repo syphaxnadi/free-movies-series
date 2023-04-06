c527a0e550a4776afd8c8c3e480b7e7e4d114f88
syphaxnadi <128228494+syphaxnadi@users.noreply.github.com>
2023-04-06 21:28:07 +0100

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,7 @@
+const loginButton = document.getElementById('login-button');
+vloginButton.addEventListener('click', () => {
+	// Handle login functionality here
+  });
+  function redirectToLoginPage() {
+	window.location.href = "web/moviespage.html";
+  }
